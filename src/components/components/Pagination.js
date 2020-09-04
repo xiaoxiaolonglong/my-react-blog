e12ed03ec2e10925e4abfd228b92e21119b84ce9
longzh <longzh@elane.cn>
2020-09-04 17:47:12 +0800

    增加分页器组件

--- src/components/components/Pagination.js
+++ src/components/components/Pagination.js
@@ -5,24 +5,21 @@ class Pagination extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pageindex:1,
+            pageindex:9,
             pagesize:10,
             pagecount:100,
-            showPage:'none'
         };
         this.nextPage = this.nextPage.bind(this);
         this.prePage = this.prePage.bind(this);
+        // this.goPage = this.goPage.bind(this);
     }
     componentDidMount() {
-        if(Math.ceil(this.state.pagecount/this.state.pagesize)>1){
-            this.setState({
-                showPage:'block'
-            })
-        }else{
-            this.setState({
-                showPage:'none'
-            })
-        }
+
+    }
+    goPage(page){
+        this.setState({
+            pageindex:page
+        })
     }
     prePage(){
         this.setState({
@@ -39,13 +36,42 @@ class Pagination extends React.Component {
         
     }
     render() {
+        let arr = [];
+        for(let i = 1;i<=Math.ceil(this.state.pagecount/this.state.pagesize);i++){
+            arr.push(i)
+        }
         return (
-            <div className="pagination" style={{display:this.state.showPage}}>
+            <div className="pagination" style={{display:this.state.pagecount>this.state.pagesize?'block':'none'}}>
                 <ul>
-                    <li className="prev-page" style={{display:this.state.pageindex==1?'none':'inline'}}><a onClick={this.prePage}>上一页</a></li>
-                    <li className="active"><span>{this.state.pageindex}</span></li>
-                    <li style={{display:this.state.pageindex*this.state.pagesize>=this.state.pagecount?'none':'inline'}}><a >{this.state.pageindex+1}</a></li>
-                    <li className="next-page" style={{display:this.state.pageindex==Math.ceil(this.state.pagecount/this.state.pagesize)?'none':'inline'}}> <a onClick={this.nextPage}>下一页</a></li>
+                    <li className="prev-page" style={{display:this.state.pageindex===1?'none':'inline'}}>
+                        <a onClick={this.prePage}>上一页</a>
+                    </li>
+                    {
+                        arr.map((item,index)=>{
+                            if(this.state.pageindex === 1){
+                                if(index > 2){
+                                    return false
+                                }
+                            }else if(this.state.pageindex === arr[arr.length-1]){
+                                if(index<arr.length-3){
+                                    return false
+                                }
+                            }else{
+                                if(index < arr.indexOf(this.state.pageindex)-1 || index > arr.indexOf(this.state.pageindex)+1){
+                                    return false
+                                }
+                            }
+                            if(item==this.state.pageindex){
+                                return <li key={index} className="active"><span>{item}</span></li>
+                            }else{
+                                return <li key={index}><a onClick={this.goPage.bind(this,item)}>{item}</a></li>
+                            }
+                            
+                        })
+                    }
+                    <li className="next-page" style={{display:this.state.pageindex===arr[arr.length-1]?'none':'inline'}}>
+                        <a onClick={this.nextPage}>下一页</a>
+                    </li>
                 </ul>
             </div>
         );
